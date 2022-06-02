971363e7e5062eaa2557df688970beb85f740c58
Marumo-ProG <Lennythobejane@gmail.com>
2022-06-02 21:17:01 +0200

    changed the db connection and SQL statements to proper database attributes

--- db-api.php
+++ db-api.php
@@ -12,7 +12,7 @@
       private function __construct()
       {
           // connecting to the database
-          $this->conn = new mysqli("127.0.0.1:3306","root","20485001","hockey_test_db");
+          $this->conn = new mysqli("127.0.0.1:3306","root","20485001","field_hockey");
           if($this->conn == false){
               die("error connecting to the database");
           }else{
@@ -36,7 +36,7 @@
       public function signUser($first_name, $last_name, $email, $password, $userType)
       {
           // registering the user and storing their information in our database
-          $query = "INSERT INTO users (first_name, last_name, email, password, is_admin) VALUES ('$first_name', '$last_name','$email','$password', '$userType')";
+          $query = "INSERT INTO users (First_Name, Last_Name, Email, Password, is_admin) VALUES ('$first_name', '$last_name','$email','$password', '$userType')";
           
           if ($this->conn->query($query) === TRUE) {
             echo "<script>New record created successfully</script>";
@@ -46,7 +46,7 @@
       }
       // this function will be used to retrieve a user using email
       public function retrieveUser($email){
-        $query = "SELECT * FROM users WHERE email='$email'";
+        $query = "SELECT * FROM users WHERE Email='$email'";
           
         $result = $this->conn->query($query);
 
@@ -55,7 +55,7 @@
       // this function will be used to check if a person can login or not using their email and password
       public function login($email, $password){
           // this function will handle user login functionality
-          $query = "SELECT * FROM users WHERE email='$email'";
+          $query = "SELECT * FROM users WHERE Email='$email'";
           
           $result = $this->conn->query($query);
 
@@ -63,7 +63,7 @@
           if($result->num_rows > 0){
             // compare the password
             $result = $result->fetch_assoc();
-            if(password_verify($password, $result["password"])){
+            if(password_verify($password, $result["Password"])){
                 return true;
             }
           }
@@ -72,7 +72,7 @@
       }
       // this funtion will be used to delete a user from the database
       public function removeUser($email){
-        $query = "DELETE FROM users WHERE email='$email'";
+        $query = "DELETE FROM users WHERE Email='$email'";
         if($this->retrieveUser($email)->num_rows > 0 && $this->conn->query($query) == true){
             echo "user deleted successfully";
         } else{
